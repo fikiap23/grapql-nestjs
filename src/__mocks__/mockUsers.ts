8ab203d2be1eadb628006a540ca3d8ffcc4f3d4f
fikiaprian23 <fikiaprian23@gmail.com>
2024-02-18 19:47:45 +0700

    add get user by id

---
+++ src/__mocks__/mockUsers.ts
@@ -0,0 +1,57 @@
+export const mockUsers = [
+  {
+    id: 1,
+    username: 'fikiap23',
+    displayName: 'Fiki Aprian',
+  },
+  {
+    id: 2,
+    username: 'dummyUser1',
+    displayName: 'Dummy User 1',
+  },
+  {
+    id: 3,
+    username: 'dummyUser2',
+    displayName: 'Dummy User 2',
+  },
+  {
+    id: 4,
+    username: 'dummyUser3',
+    displayName: 'Dummy User 3',
+  },
+  {
+    id: 5,
+    username: 'dummyUser4',
+    displayName: 'Dummy User 4',
+  },
+  {
+    id: 6,
+    username: 'dummyUser5',
+    displayName: 'Dummy User 5',
+  },
+  {
+    id: 7,
+    username: 'dummyUser6',
+    displayName: 'Dummy User 6',
+  },
+  {
+    id: 8,
+    username: 'dummyUser7',
+    displayName: 'Dummy User 7',
+  },
+  {
+    id: 9,
+    username: 'dummyUser8',
+    displayName: 'Dummy User 8',
+  },
+  {
+    id: 10,
+    username: 'dummyUser9',
+    displayName: 'Dummy User 9',
+  },
+  {
+    id: 11,
+    username: 'dummyUser10',
+    displayName: 'Dummy User 10',
+  },
+];
--- src/graphql/resolvers/UserResolver.ts
+++ src/graphql/resolvers/UserResolver.ts
@@ -1,14 +1,11 @@
-import { Query, Resolver } from '@nestjs/graphql';
+import { Args, Int, Query, Resolver } from '@nestjs/graphql';
 import { User } from '../models/User';
+import { mockUsers } from 'src/__mocks__/mockUsers';
 
 @Resolver('User')
 export class UserResolver {
   @Query((returns) => User)
-  getUser() {
-    return {
-      id: 1,
-      username: 'test',
-      displayName: 'test display name',
-    };
+  getUserById(@Args('id', { type: () => Int }) id: number) {
+    return mockUsers.find((user) => user.id === id);
   }
 }
