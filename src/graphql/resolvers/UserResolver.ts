c1aacca51cdf9a1f460b88a3d094fa41f3e58925
fikiaprian23 <fikiaprian23@gmail.com>
2024-02-18 20:53:00 +0700

    add mutation create user

--- src/graphql/resolvers/UserResolver.ts
+++ src/graphql/resolvers/UserResolver.ts
@@ -1,6 +1,7 @@
 import {
   Args,
   Int,
+  Mutation,
   Parent,
   Query,
   ResolveField,
@@ -10,6 +11,8 @@ import { User } from '../models/User';
 import { mockUsers } from 'src/__mocks__/mockUsers';
 import { mockUserSettings } from 'src/__mocks__/mockUserSettings';
 import { UserSetting } from '../models/UserSetting';
+
+let incrementId = mockUsers.length + 1;
 
 @Resolver((of) => User)
 export class UserResolver {
@@ -27,5 +30,19 @@ export class UserResolver {
   getUserSetting(@Parent() user: User) {
     // console.log(user);
     return mockUserSettings.find((setting) => setting.userId === user.id);
+  }
+
+  @Mutation((returns) => User)
+  createUser(
+    @Args('username') username: string,
+    @Args('displayName', { nullable: true }) displayName: string,
+  ) {
+    const newUser = {
+      id: incrementId++,
+      username,
+      displayName,
+    };
+    mockUsers.push(newUser);
+    return newUser;
   }
 }
