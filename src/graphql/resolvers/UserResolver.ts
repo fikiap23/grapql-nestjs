a48154ec0709d29e674fad16e6c94128e0b6a828
fikiaprian23 <fikiaprian23@gmail.com>
2024-02-18 19:50:51 +0700

    add name

--- src/graphql/resolvers/UserResolver.ts
+++ src/graphql/resolvers/UserResolver.ts
@@ -4,7 +4,7 @@ import { mockUsers } from 'src/__mocks__/mockUsers';
 
 @Resolver('User')
 export class UserResolver {
-  @Query((returns) => User)
+  @Query((returns) => User, { nullable: true, name: 'userById' })
   getUserById(@Args('id', { type: () => Int }) id: number) {
     return mockUsers.find((user) => user.id === id);
   }
